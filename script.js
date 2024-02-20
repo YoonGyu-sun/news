4f0427097c795641a2c3dd580540ae4c7d6d51e9
YoonGyu-sun <YoonGyu-sun>
2024-02-20 23:41:42 +0900

    4th news

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = `34b60cd6736e43abb79ca9f9d2bf6b2b`
 let news = [];
 const getLatestNews = async()=>{
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?page=1&pageSize=20&apiKey=${API_KEY}`); 
+    const url = new URL(`https://newsjs.netlify.app/top-headlines?page=1&pageSize=20&apiKey=${API_KEY}`); 
     const response = await fetch(url);
     const data = await response.json();
     console.log(response);

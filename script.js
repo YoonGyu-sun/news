6a434217bdf2bb6a3c18ed0ba54df2352e383797
YoonGyu-sun <YoonGyu-sun>
2024-02-20 23:31:51 +0900

    2th news

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const API_KEY = `34b60cd6736e43abb79ca9f9d2bf6b2b`
 let news = [];
 const getLatestNews = async()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-
+    const url = new URL(`https://newsjs.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`); 
     const response = await fetch(url);
     const data = await response.json();
     console.log(response);

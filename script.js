696ebef4ec452d748712124651c00db03ab691fc
YoonGyu-sun <YoonGyu-sun>
2024-02-22 01:01:21 +0900

    7th news

--- script.js
+++ script.js
@@ -16,12 +16,20 @@ const render=()=>{
     const newsHTML = newsList.map
     (news=> ` <div class="row news">
     <div class="col-lg-4">
-        <img class="news-img-size" src="${news.urlToImage}"/>
+        <img class="news-img-size" src="${news.urlToImage ||
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+}"/>
     </div>
     <div class="col-lg-8">
         <h2>${news.title}</h2>
-        <p>${news.description}</p>
-        <div>${news.source.name}</div>
+        <p>${
+            news.description == null || news.description == ""
+              ? "내용없음"
+              : news.description.length > 200
+              ? news.description.substring(0, 200) + "..."
+              : news.description
+       }</p>
+        <div>${news.source.name || "no source" } </div>
     </div>
 </div>`).join('');
     
